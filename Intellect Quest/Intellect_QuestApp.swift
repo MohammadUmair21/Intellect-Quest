8e70bd204f7408d25883518fdab17829fe10719e
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-07-24 15:02:18 +0530

    Initial Commit

---
+++ Intellect Quest/Intellect_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intellect_QuestApp.swift
+//  Intellect Quest
+//
+//  Created by Umair on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Intellect_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
